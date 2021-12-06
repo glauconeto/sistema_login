c387c90a06a1f1c925bf9813f08c7dfdff6ab3ed
glauconeto <neto.glauquinho@gmail.com>
2021-12-06 17:40:56 -0300

    front-end finalizado

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="box_login">
+        <div class="cover"></div>
+        <h1>Faça seu login</h1>
+
+        <form action="">
+            <input type="email" name="e-mail">
+            <input type="password" name="password">
+
+            <button class="btn_login">LOGAR</button>
+        </form>
+    </div>
+</body>
+</html>
